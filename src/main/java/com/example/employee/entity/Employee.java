cf7f8b2e13a7d8c6e9be5d60857375e456c97966
fishsalted <10572677+fishsalted@users.noreply.github.com>
2019-04-20 14:41:21 +0800

    添加 set 方法和默认构造

--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -1,13 +1,29 @@
 package com.example.employee.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Employee")
 public class Employee {
 
+    @Id
     int id;
     String name;
     int age;
     String gender;
     int companyId;
     int salary;
+
+    public Employee() {
+        this.id = -1;
+        this.name = "";
+        this.age = -1;
+        this.gender = "";
+        this.companyId = -1;
+        this.salary = -1;
+    }
 
     public Employee(int id,String name, int age, String gender, int companyId, int salary) {
         this.id = id;
@@ -21,24 +37,42 @@ public class Employee {
     public int getId() {
         return id;
     }
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
+    }
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getAge() {
         return age;
     }
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     public String getGender() {
         return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
     }
 
     public int getCompanyId() {
         return companyId;
     }
+    public void setCompanyId(int companyId) {
+        this.companyId = companyId;
+    }
 
     public int getSalary() {
         return salary;
+    }
+    public void setSalary(int salary) {
+        this.salary = salary;
     }
 }
