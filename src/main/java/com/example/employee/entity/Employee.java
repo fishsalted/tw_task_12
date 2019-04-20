d4241cae00df0d46dea48559718df5ccefb76a63
fishsalted <10572677+fishsalted@users.noreply.github.com>
2019-04-20 09:09:20 +0800

    完成 Employee

--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -2,4 +2,43 @@ package com.example.employee.entity;
 
 public class Employee {
 
+    int id;
+    String name;
+    int age;
+    String gender;
+    int companyId;
+    int salary;
+
+    public Employee(int id,String name, int age, String gender, int companyId, int salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.companyId = companyId;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
 }
